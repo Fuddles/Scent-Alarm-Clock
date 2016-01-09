41ce5a6723ee39fd9abb5722a33f60f864b9f75d
Eric <eric.amram@gmail.com>
2016-01-09 15:55:49 -0800

    1st test of screen

--- ArduinoSketchFolder/scent-alarm-clock/scent-alarm-clock.ino
+++ ArduinoSketchFolder/scent-alarm-clock/scent-alarm-clock.ino
@@ -1,9 +1,19 @@
+
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment ledScreen = Adafruit_7segment();
+
+
 void setup() {
   // put your setup code here, to run once:
 
-  print(1234);
-  drawColon(true);
-  writeDisplay();
+  ledScreen.setBrightness(15);     // Fully bright
+
+  ledScreen.print(1234);
+  ledScreen.drawColon(true);
+  ledScreen.writeDisplay();
 
 }
 
