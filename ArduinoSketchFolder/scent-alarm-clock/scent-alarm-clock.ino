5d7141fed0e75b2c66156dd8b30cb3eac1c36a85
Eric <eric.amram@gmail.com>
2016-01-09 18:43:19 -0800

    Update

--- ArduinoSketchFolder/scent-alarm-clock/scent-alarm-clock.ino
+++ ArduinoSketchFolder/scent-alarm-clock/scent-alarm-clock.ino
@@ -1,13 +1,25 @@
+/**
+ *  Alarm clock project that turns a fan on when the alarm triggers
+ */
 
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+// Comment the line to remove the console debugging (via Serial)
+#define DEBUG         true
+
+// Buffer size to print on the console
+#define BUFFER_SIZE   2048
+char    buffer[BUFFER_SIZE];
+
+// ----- Inits for the RTC clock
+#include <Wire.h>
+#include "ds3231.h"
+struct ts rtcTime;
+
+// ----- Inits for the screen
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
-#include "ds3231.h"
 
-
-Adafruit_7segment ledScreen = Adafruit_7segment();
-
-boolean middlecolon = true;
+Adafruit_7segment ledScreen         = Adafruit_7segment();
+boolean           middleColonToggle = false;
 
 // TODO: Doc is wrong about writeDigitNum(location, number, dot)
 // 0x02 - center colon
@@ -15,26 +27,129 @@ boolean middlecolon = true;
 // 0x08 - left colon - upper dot
 // 0x10 - decimal point
 
-void setup() {
-  // put your setup code here, to run once:
-  ledScreen.begin(0x70);
-  ledScreen.setBrightness(15);     // Fully bright
 
-  ledScreen.print(1234, DEC);
-  ledScreen.drawColon(true);
-  ledScreen.writeDisplay();
- 
+// ----- Inits for the DC motor
+// TODO
+
+// ----- Inits for the buttons
+// TODO
+
+// Two buttons pressed for 5secs should turn the alarm on to test
+
+
+
+
+// =============================================================================================
+// ------ SETUP & LOOP ------
+// =============================================================================================
+
+/**
+ *  Once, at start
+ */
+void setup() 
+{
+    // Console log
+    Serial.begin(9600);
+
+    // RTC 
+    Wire.begin();
+    DS3231_init(DS3231_INTCN);
+    DS3231_clear_a1f();               // FIXME: should not reset the alarm but keep previous
+
+    // Screen
+    ledScreen.begin(0x70);
+    ledScreen.setBrightness(15);     // Fully bright
+
+    // TODO: DC Motor
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  delay(1000);
-  middlecolon = !middlecolon;
-  // ledScreen.drawColon( middlecolon );
 
-  // --- Which dots on?
-////  ledScreen.writeDigitNum(2, 0, true ); // lower + upper + decimal (all)
+/**
+ *  Endless loop. Will try to run every second, steadily at '000'ms
+ */
+void loop() 
+{
+    unsigned long loopStartMs = millis();
+
+    // --- Get time and display it
+    DS3231_get( &rtcTime );
+#ifdef DEBUG
+    // display the time on the console
+    snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", 
+            rtcTime.year, rtcTime.mon, rtcTime.mday, rtcTime.hour, rtcTime.min, rtcTime.sec);
+    Serial.println(buff);
+#endif
+    // display the time on the screen, toggling the middle colon every second
+    ledScreen.print( rtcTime.hour*100 + rtcTime.min, DEC);
+    middleColonToggle = !middleColonToggle;
+    ledScreen.drawColon( middlecolon );
+    ledScreen.writeDisplay();
+
+    // TODO: dots for AM/PM (or 24h format?)
+    // TODO: dot when alarm set
+
+    // Which dots on?
+    //  ledScreen.writeDigitNum(2, 1); // upper
+    //  ledScreen.writeDigitNum(2, 3); // upper + lower
+    //  ledScreen.writeDigitNum(2, 0); // upper + lower + decimal
+
+
+
+    // --- Check buttons
+
+
+
+    // 
+
+
+  
+
+    // --- To loop every second. 
+    //      If took more than 1 sec, loop immediately
+    //      Otherwise try to loop until the next second precisely (000ms)
+    unsigned long loopEndMs = millis();
+    if ( loopEndMs - loopStartMs < 999 ) {
+        int diff = 999 - (loopEndMs % 1000);
+        delay( diff );
+    }
+    return;
+}
+
+
+
+// =============================================================================================
+// ++++++ HELPER FUNCTIONS ++++++
+// =============================================================================================
+
+/**
+ * Set the alarm clock
+ */
+void set_alarm(uint8_t hour, uint8_t minute, uint8_t second)
+{
+    // flags define what calendar component to be checked against the current time in order
+    // to trigger the alarm - see datasheet
+    // A1M1 (seconds) (0 to enable, 1 to disable)
+    // A1M2 (minutes) (0 to enable, 1 to disable)
+    // A1M3 (hour)    (0 to enable, 1 to disable) 
+    // A1M4 (day)     (0 to enable, 1 to disable)
+    // DY/DT          (dayofweek == 1/dayofmonth == 0)
+    uint8_t flags[5] = { 0, 0, 0, 1, 1 };
+
+    // set Alarm1
+    DS3231_set_a1( second, minute, hour, 0, flags);
+
+    // activate Alarm1
+    DS3231_set_creg(DS3231_INTCN | DS3231_A1IE);
+}
+
+
+
+
+
+
+/*
+ ////  ledScreen.writeDigitNum(2, 0, true ); // lower + upper + decimal (all)
 ////  ledScreen.writeDigitNum(2, 1, true ); // upper
 ////  ledScreen.writeDigitNum(2, 2, true ); // lower + decimal
 ////  ledScreen.writeDigitNum(2, 3, true ); // lower + upper
@@ -42,8 +157,5 @@ void loop() {
 ////  ledScreen.writeDigitNum(2, 5, true ); // lower + upper
 ////  ledScreen.writeDigitNum(2, 6, true ); // lower + upper + decimal (all)
 ////  ledScreen.writeDigitNum(2, 7, true ); // upper
-ledScreen.writeDigitNum(2, 0x08, true ); // lower + upper + decimal (all)
-  
-  ledScreen.writeDisplay();
-
-}
+*/
+ */
